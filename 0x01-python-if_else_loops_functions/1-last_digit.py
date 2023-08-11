dbf5384014034d4f3742a3a7b3c6fea853bbde06
osfoce <oselufortunatus2002@gmail.com>
2023-08-11 19:40:30 +0100

    The last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 # using formular for calculating remainder
-#https://www.designcise.com/web/tutorial/how-to-get-the-last-digit-of-an-integer-in-python#:~:text=The%20modulo%20operator%20in%20Python,then%20add%20the%20sign%20back.
+#https://www.designcise.com/web/tutorial/how-to-get-the-last-digit-of-an-integer
+#-in-python#:~:text=The%20modulo%20operator%20in%20Python,then%20add%20the%20sign%20back.
 last_digit = (number - (10 * int(number / 10)))
 
 if last_digit > 5:
