fec17ce01cb2b69d815744a5195e63d89ceaaf41
osfoce <oselufortunatus2002@gmail.com>
2023-08-11 21:25:38 +0100

    str lower fxn

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) >= ord(97) and ord(c) <= ord(122)
+    if ord(c) >= ord(97) and ord(c) <= ord(122):
         return True
-    elif ord(c) >= ord(65) and ord(c) <= ord(90)
+    elif ord(c) >= ord(65) and ord(c) <= ord(90):
         return False
