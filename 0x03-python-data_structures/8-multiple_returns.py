0b3c501592faaacfb09cb738c9739dd76f7cf7ee
osfoce <oselufortunatus2002@gmail.com>
2023-08-14 21:58:54 +0100

    edit tuple multi return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    my_tuple = ()
     length = len(sentence)
-    if sentence[0] == 0:
-        return None
+    if length == 0:
+        my_tuple = 0, None
     else:
         first_string = sentence[0]
+        my_tuple = length, first_string
     return length, first_string
-
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
-
