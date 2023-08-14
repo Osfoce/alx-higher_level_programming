813208a03e646071f462b6a2a277140c1b96389e
osfoce <oselufortunatus2002@gmail.com>
2023-08-14 23:14:39 +0100

    del

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -6,9 +6,3 @@ def delete_at(my_list=[], idx=0):
         new_list = my_list[:idx] + my_list[idx+1:]
     return new_list
 
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
-
