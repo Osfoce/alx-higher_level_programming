7ab27420a4ff2cf9ac757fc1d9ccddced8d0f8a9
osfoce <oselufortunatus2002@gmail.com>
2023-08-14 20:31:50 +0100

    a fxn that removes a character from a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
     return my_string.translate({ord(i): None for i in 'cC'})
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
-
