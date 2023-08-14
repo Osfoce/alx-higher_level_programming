a149ba4e7ff364c991e6df8066fc4ec8cf8a33a6
osfoce <oselufortunatus2002@gmail.com>
2023-08-14 21:39:08 +0100

    tupple addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tmp_tuple = ()
-    tuple_1 = tuple_a + (0, 0)
-    tuple_2 = tuple_b + (0, 0)
-    new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
-    return tmp_tuple
+# Ensure both tuples have at least 2 elements
+    while len(tuple_a) < 2:
+        tuple_a = tuple_a + (0,)
+    while len(tuple_b) < 2:
+        tuple_b = tuple_b + (0,)
+# Take only the first 2 elements of each tuple
+    tuple_a = tuple_a[:2]
+    tuple_b = tuple_b[:2]
+# Add the tuples element-wise
+    result = tuple(x + y for x, y in zip(tuple_a, tuple_b))
+    return result
