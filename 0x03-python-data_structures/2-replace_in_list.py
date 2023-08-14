f3bd12ecdb49b3a298dc98de1005bd41a31fe83d
osfoce <oselufortunatus2002@gmail.com>
2023-08-14 19:57:54 +0100

    Replace in a cpy

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,5 +2,13 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0 and idx > len(my_list):
         return my_list
+    new_list = my_list
     my_list[idx] = element
-    return (0)
+    return new_list
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 9
+new_list = replace_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)
